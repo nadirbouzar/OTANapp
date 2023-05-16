2898df151e906195a6f9f03594f9ce55fd467b48
Djawad <sbell5122@gmail.com>
2023-05-16 11:16:09 +0200

    menu complet

--- app/src/main/java/com/example/otanapp/MainActivity.java
+++ app/src/main/java/com/example/otanapp/MainActivity.java
@@ -6,14 +6,41 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.Button;
+import android.content.Intent;
 
 import com.google.android.material.button.MaterialButton;
 
 import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
+    private Button login ;
 
     @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        //super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_main);
+
+        //TextView  ID = (TextView) findViewById(R.id.ID);
+        //TextView password = (TextView) findViewById(R.id.password);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        login = findViewById(R.id.login);
+        login.setOnClickListener(new View.OnClickListener() {
+            Button login = findViewById(R.id.login);
+
+            public void onClick(View v) {
+                // Navigate to the second activity
+                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
+
+
+   /* @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
@@ -21,21 +48,24 @@ public class MainActivity extends AppCompatActivity {
         TextView  ID = (TextView) findViewById(R.id.ID);
         TextView password = (TextView) findViewById(R.id.password);
 
-        MaterialButton login = (MaterialButton) findViewById(R.id.login);
+        Button login = findViewById(R.id.login);
 
         //ID : userA et password : alibaba123
+
 
         login.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-                if (ID.getText().toString().equals("userA") && password.getText().toString().equals("alibaba123")) {
+                //if (ID.getText().toString().equals("userA") && password.getText().toString().equals("alibaba123")) {
                     //correct
-                    Toast.makeText(MainActivity.this, "LOGIN SUCCESSFULL", Toast.LENGTH_SHORT).show();
-                }else
+                    //Toast.makeText(MainActivity.this, "LOGIN SUCCESSFULL", Toast.LENGTH_SHORT).show();
+                //}else
                     //incorrect
-                    Toast.makeText(MainActivity.this, "LOGIN UNSUCCESSFULL", Toast.LENGTH_SHORT).show();
+                    //Toast.makeText(MainActivity.this, "LOGIN UNSUCCESSFULL", Toast.LENGTH_SHORT).show();
+                //Intent intent = new Intent(MainActivity.this, HomeFragment.class);
+                //startActivity(intent);
             }
         });
 
     }
-}
+}*/
---
+++ app/src/main/java/com/example/otanapp/MainActivity2.java
@@ -0,0 +1,93 @@
+package com.example.otanapp;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.ListFragment;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.navigation.NavigationView;
+
+
+public class MainActivity2 extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    private DrawerLayout drawerLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open_nav,
+                R.string.close_nav);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
+            navigationView.setCheckedItem(R.id.nav_home);
+
+        }
+
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.nav_home:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
+                break;
+
+            case R.id.nav_text:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new TextFragment()).commit();
+                break;
+
+            case R.id.nav_window:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new WindowFragment()).commit();
+                break;
+
+            case R.id.nav_logout:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new LogoutFragment()).commit();
+                break;
+
+            case R.id.nav_map:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new MapFragment()).commit();
+                break;
+
+            case R.id.nav_list:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new ListFragment()).commit();
+                break;
+
+            case R.id.nav_account:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new AccountFragment()).commit();
+                break;
+            case R.id.nav_info:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new InfoFragment()).commit();
+                break;
+
+
+        }
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
